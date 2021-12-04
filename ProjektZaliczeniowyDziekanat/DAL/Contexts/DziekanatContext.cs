0c714311f668065897b241227e9d5c73633de3a2
oiskra <ojaskra@gmail.com>
2021-12-04 16:57:09 +0100

    Rozszerzenia entity, dodanie Dbcontext

---
+++ ProjektZaliczeniowyDziekanat/DAL/Contexts/DziekanatContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace ProjektZaliczeniowyDziekanat.DAL.Contexts
+{
+    public class DziekanatContext : DbContext 
+    {
+
+        public DziekanatContext(DbContextOptions<DziekanatContext> options) : base(options)
+        {
+
+        }
+
+
+    }
+}
